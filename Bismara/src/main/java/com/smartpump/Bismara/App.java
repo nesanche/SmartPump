cefdacb2266d0d2fbd0d771458301ced96799e09
luqi <lucas_borsatto@hotmail.com>
2015-04-16 16:55:40 -0300

    Subo Bismara Project.

---
+++ Bismara/src/main/java/com/smartpump/Bismara/App.java
@@ -0,0 +1,13 @@
+package com.smartpump.Bismara;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
