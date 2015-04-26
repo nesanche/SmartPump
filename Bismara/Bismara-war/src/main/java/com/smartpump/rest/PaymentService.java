ad2e3234fb36518a2db539ebf42d1eea448dae4f
luqi <lucas_borsatto@hotmail.com>
2015-04-25 22:36:05 -0300

    Subo

---
+++ Bismara/Bismara-war/src/main/java/com/smartpump/rest/PaymentService.java
@@ -0,0 +1,27 @@
+package com.smartpump.rest;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.smartpump.transaction.TransactionBo;
+
+@Component
+@Path("/payment")
+public class PaymentService {
+	@Autowired
+	TransactionBo transactionBo;
+ 
+	@GET
+	@Path("/bismara")
+	public Response savePayment() {
+ 
+		String result = transactionBo.save();
+ 
+		return Response.status(200).entity(result).build();
+ 
+	}
+}
